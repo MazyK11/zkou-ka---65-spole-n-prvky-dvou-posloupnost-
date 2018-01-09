3e1c1d301260fce9d65b150e64d6e2f3228eb121
MazyK11 <33461811+MazyK11@users.noreply.github.com>
2018-01-09 16:31:56 +0100

    min

--- intersect/src/intersect/Intersect.java
+++ intersect/src/intersect/Intersect.java
@@ -5,6 +5,7 @@
  */
 package intersect;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -21,6 +22,9 @@ public class Intersect {
         int p [][] = new int [2][(int) Math.pow(10,k)];
         rnd(k,p);
         
+        Arrays.sort(p[0]);
+        Arrays.sort(p[1]);
+        
         for(int i = 0;i<p[0].length;i++){
             System.out.format("%d ",p[0][i]);
         }
@@ -28,6 +32,11 @@ public class Intersect {
         for(int i = 0;i<p[1].length;i++){
             System.out.format("%d ",p[1][i]);
         }
+        
+        int min = min(p);
+        System.out.print("\n");
+        System.out.print(min);
+      
     }
     /** Metoda pro generování náhodných čísel
      * @param k - exponent
@@ -40,5 +49,29 @@ public class Intersect {
             p[0][i] = numbers.nextInt(100);
             p[1][i] = numbers.nextInt(100);
         }
+    }
+    /** Metoda pro výběr vhodnější posloupnosti pro hledání
+     * @param p - dvourozměrné pole reprezentující posloupnosti
+     * @return číslo sudé nebo liché, které označuje vhodnější posloupnost
+     */  
+    public static int min(int p[][]){
+        int i;
+//      větší minimum -> prvky dané posloupnosti budo hledány ve druhé
+        if (p[0][0] > p[1][0]){
+            i = 0;
+        }
+        else if (p[0][0] < p[1][0]){
+            i=1;
+        }
+        else {
+//          menší maximum -> prvky dané posloupnosti budo hledány ve druhé
+            if(p[0][p[0].length-1] < p[1][p[1].length-1]){
+                i = 2;
+            }
+            else {
+                i = 3;
+            }
+        }
+        return i;
     }
 }
