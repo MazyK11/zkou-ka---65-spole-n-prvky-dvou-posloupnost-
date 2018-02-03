4753bb3d2f2d0a715edadf1a37e3e6a9463f21ae
MazyK11 <33461811+MazyK11@users.noreply.github.com>
2018-02-03 01:36:13 +0100

    zmatení

--- intersect/src/intersect/Intersect.java
+++ intersect/src/intersect/Intersect.java
@@ -39,14 +39,16 @@ public class Intersect {
         System.out.print("\n");
         System.out.print(minmax[1]);
         int exist[] = {0};
-        int idapozice[] = vhodnost(p,minmax,exist);
+        int t[] = {-1};
+        int a[] = {0};
+        int idapozice[] = vhodnost(p,minmax,exist,t,a);
         System.out.print("\n");
         System.out.print(idapozice[0]);
         System.out.print("\n");
         System.out.print(idapozice[1]);
         System.out.print("\n");
         System.out.print(exist[0]);
-        intersect(p,exist,idapozice);
+        intersect(p,exist,idapozice,t,a);
       
     }
     /** Metoda pro generování náhodných čísel
@@ -57,8 +59,8 @@ public class Intersect {
         Random numbers = new Random();
         for(int i =0;i<Math.pow(10,k);i++){
 //          interval čísel pro generování 
-            p[0][i] = numbers.nextInt(100);
-            p[1][i] = numbers.nextInt(100);
+            p[0][i] = numbers.nextInt(10);
+            p[1][i] = numbers.nextInt(10);
         }
     }
     /** Metoda, která charakterizuje vztahy mezi minimy a maximy posloupností
@@ -97,7 +99,8 @@ public class Intersect {
      * @return - pole s informací o vhodnější posloupnosti a případně s pozicí
      * menšího maxima v druhé posloupnosti
      */  
-    public static int []vhodnost(int p[][], int minmax[], int exist[]){
+    public static int []vhodnost(int p[][], int minmax[], int exist[], int t[],
+            int a[]){
         int idaprvek[] = new int [2];
         idaprvek[1] = -1;
         int j=0;
@@ -109,14 +112,14 @@ public class Intersect {
                 int r = p[1].length-1;
                 int m = p[0][p[0].length-1];
                 j++;
-                idaprvek[1] = vyhledavani(p,l,r,m,j,exist);
+                idaprvek[1] = vyhledavani(p,l,r,m,j,exist,t,a);
                 idaprvek[0] = 1;
             }
             else if(minmax[1] == 1){
                 int l = 0;
                 int r = p[0].length-1;
                 int m = p[1][p[1].length-1];
-                idaprvek[1] = vyhledavani(p,l,r,m,j,exist);
+                idaprvek[1] = vyhledavani(p,l,r,m,j,exist,t,a);
                 idaprvek[0] = 0;
             }
         }
@@ -132,7 +135,7 @@ public class Intersect {
                         int l = 0;
                         int r = p[i].length-1;                      
                         int m = p[1-i][p[1-i].length-1];
-                        idaprvek[1] = vyhledavani(p,l,r,m,j+i,exist);
+                        idaprvek[1] = vyhledavani(p,l,r,m,j+i,exist,t,a);
                         idaprvek[0] = 0+i;
                     }
                     else {
@@ -153,13 +156,26 @@ public class Intersect {
      * @return -  pozice, do které je nutné hledat stejná čísla.
      */  
     public static int vyhledavani(int p[][], int l, int r, int m,int j,
-            int [] exist){
+            int [] exist, int [] t, int [] a){
         int k = (l+r)/2;
         if(r-l <= 1){
             if (p[0+j][r]==m){
                 return r;
             }
             else if(p[0+j][l]==m){
+                if (t[0] == l){
+                    a[0]++;
+                    if(0 <= l-a[0]){
+                        if(p[0+j][l]==p[0+j][l-a[0]]){
+                            return l-a[0];
+                        }
+                    }    
+                    if(p[0+j][l]==p[0+j][l+1]){
+                    p[j][l] = p[j][0] -1;                    
+                        return l+1;
+                    }
+                    exist[0] = -1;
+                }
                 return l;
             }
             else {
@@ -170,16 +186,30 @@ public class Intersect {
             }
         }
         if(m == p[0+j][k]){
+            if (t[0] == k){
+                a[0]++;
+                if(0 <= k-a[0]){
+                    if(p[0+j][k]==p[0+j][k-a[0]]){
+                    return k-a[0];
+                    }
+                }
+                if(p[0+j][k]==p[0+j][k+1]){
+                p[j][k] = p[j][0] -1;                    
+                    return k+1;
+                }
+                exist[0] = -1;
+            }
             return k;
         }
         else if (m > p[0+j][k]){
-            return vyhledavani(p,k,r,m,j,exist);
+            return vyhledavani(p,k,r,m,j,exist,t,a);
         }
         else {
-            return vyhledavani(p,l,k,m,j,exist);
+            return vyhledavani(p,l,k,m,j,exist,t,a);
         }  
     }
-    public static void intersect(int p[][],int exist [], int idapozice []){
+    public static void intersect(int p[][],int exist [], int idapozice [],
+            int t[], int []a){
         int l = 0;int r;int j =0;int mmax;int g =0;
         if (idapozice[0] == 0){
             j++;
@@ -206,10 +236,14 @@ public class Intersect {
         for(int i =0;i<mmax;i++){
             exist[0] = -2;
             int m = p[1-j][i];
-            int h = vyhledavani(p,l,r,m,j,exist);
+            int h = vyhledavani(p,l,r,m,j,exist,t,a);
             if (exist[0] != -1){
                 intersect [i-g] = p[j][h];
-                p[j][h] = p[j][0];    
+//                p[j][h] = p[j][0];
+                if(t[0] != h+a[0]){
+                    a[0] =0;
+                 }
+                 t[0] = h+a[0];
             }
             else {
                 g++;
